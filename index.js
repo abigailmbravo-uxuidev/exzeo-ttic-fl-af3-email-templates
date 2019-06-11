a04875d49f749663607890b0319a0bd8453b7b8c
John D'Amore <jdamore@Johns-iMac.local>
2019-06-11 16:05:33 -0400

    add support for command line args

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .example('$0 update rules', '(updates rules)')
+  .example('$0 update email', '(updates email templates)')
+  .example('$0 update -all', '(updates rules and email templates)')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+const updateCommand = argv._.includes('update');
+const updateAll = updateCommand ? (argv.all || false) : false;
+const updateRules = (updateAll) || (updateCommand && argv._.includes('rules'));
+const updateEmail = (updateAll) || (updateCommand && argv._.includes('email'));
+
 const service = require('exframe-service');
 service.init({ logger: require('./lib/logger'), timeout: 0 });
 
@@ -14,6 +28,7 @@ const context = {
 };
 
 Promise.all([
-  require('./lib/update-rules').updateRules(context)
+  updateRules && require('./lib/update-rules').updateRules(context),
+  updateEmail && require('./lib/update-email').updateEmail(context)
 ])
   .then(() => service.gracefulShutdown());
