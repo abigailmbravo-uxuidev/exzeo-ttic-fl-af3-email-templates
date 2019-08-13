d7ed20fa881bfb8fe57a902535d50fffb2b7ec05
Giridhar Raveendran <graveendran@outlook.com>
2019-08-13 16:00:48 -0400

    Script to update coveragedetails

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const argv = require('yargs')
   .usage('Usage: $0 <command> [options]')
   .example('$0 update rules', '(updates rules)')
   .example('$0 update email', '(updates email templates)')
-  .example('$0 update -all', '(updates rules and email templates)')
+  .example('$0 update coveragedetails', '(updates coverage details')
+  .example('$0 update --all', '(updates rules, email templates, coveragedetails)')
   .help('h')
   .alias('h', 'help')
   .argv;
@@ -13,6 +14,7 @@ const updateCommand = argv._.includes('update');
 const updateAll = updateCommand ? (argv.all || false) : false;
 const updateRules = (updateAll) || (updateCommand && argv._.includes('rules'));
 const updateEmail = (updateAll) || (updateCommand && argv._.includes('email'));
+const updateCoverageDetails = (updateAll) || (updateCommand && argv._.includes('coveragedetails'));
 
 const service = require('exframe-service');
 service.init({ logger: require('./lib/logger'), timeout: 0 });
@@ -31,6 +33,7 @@ console.log(argv);
 
 Promise.all([
   updateRules && require('./lib/update-rules').updateRules(context),
-  updateEmail && require('./lib/update-email').updateEmail(context)
+  updateEmail && require('./lib/update-email').updateEmail(context),
+  updateCoverageDetails && require('./lib/update-coveragedetails').updateCoverageDetails(context)
 ])
   .then(() => service.gracefulShutdown());
