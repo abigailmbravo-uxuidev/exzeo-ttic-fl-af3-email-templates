abb807418a98885f6c3bf09f913e6f9fd071916a
Brian Darrow <bdarrow@Helps-MacBook-Pro-3.local>
2019-12-10 09:25:45 -0500

    added rating engines

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const { argv } = require('yargs')
   .example('$0 update noteTypes', '(updates note types)')
   .example('$0 update documentevents', '(updates document event configurations)')
   .example('$0 update documentpackets', '(updates document packet configurations)')
-  .example('$0 update --all', '(updates all templates: email, rules, config, coverage, document events, document packets, form fields, and note types)')
+  .example('$0 update engines', '(updates rating engines)')
+  .example('$0 update --all', '(updates all templates: email, rules, config, coverage, document events, document packets, form fields, rating engines, and note types)')
   .help('h')
   .alias('h', 'help');
 
@@ -29,6 +30,7 @@ const updateFormFields = (updateAll) || (updateCommand && argv._.includes('form'
 const updateDocumentEvents = (updateAll) || (updateCommand && argv._.includes('documentevents'));
 const updateDocumentPackets = (updateAll) || (updateCommand && argv._.includes('documentpackets'));
 const updateNoteTypes = (updateAll) || (updateCommand && argv._.includes('noteTypes'));
+const updateRatingEngines = (updateAll) || (updateCommand && argv._.includes('engines'));
 
 service.init({ logger: require('./lib/logger'), timeout: 0 });
 taskPool.on('progress', ({ name, percentComplete }) => console.log(`${name}: ${percentComplete}`));
@@ -50,6 +52,7 @@ Promise.all([
   updateFormFields && require('./lib/update-form-fields').updateFormFields(context),
   updateDocumentEvents && require('./lib/update-documentevents').updateEvents(context),
   updateDocumentPackets && require('./lib/update-documentpackets').updatePackets(context),
-  updateNoteTypes && require('./lib/update-noteTypes').updateNoteTypes(context)
+  updateNoteTypes && require('./lib/update-noteTypes').updateNoteTypes(context),
+  updateRatingEngines && require('./lib/update-engines').updateRatingEngines(context)
 ])
   .then(() => service.gracefulShutdown());
